b638f1b5277960a863850c28f7e4047b47bee273
Aleksey F <38490271+ZomboKid@users.noreply.github.com>
2019-04-03 11:23:24 +0300

    Update revert_vm.py

--- revert_vm.py
+++ revert_vm.py
@@ -2,6 +2,7 @@
 
 from pyVim.connect import SmartConnect, Disconnect
 from pyVim.task import WaitForTasks
+from pyVmomi import vim
 import ssl
 
 s = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
@@ -12,18 +13,36 @@ snapshot_name = "init_snapshot"
 connection = SmartConnect(host="10.0.0.100", user="user1@vsphere.local",
                           pwd="passw0rd", sslContext=s)
 
-vms = connection.content.rootFolder.childEntity[0].vmFolder.childEntity[0]\
-      .childEntity[0].childEntity
+vms=[]
+
+for name in vm_names:
+    entity_stack = connection.content.rootFolder.childEntity
+    while entity_stack:
+        entity = entity_stack.pop()
+        if entity.name == name:
+            vms.append(entity)
+            del entity_stack[0:len(entity_stack)]
+        elif hasattr(entity, 'childEntity'):
+            entity_stack.extend(entity.childEntity)
+        elif isinstance(entity, vim.Datacenter):
+            entity_stack.append(entity.vmFolder)
+
+def get_snapshots_by_name_recursively(snapshots, snapname):
+    snap_obj = []
+    for snapshot in snapshots:
+        if snapshot.name == snapname:
+            snap_obj.append(snapshot)
+        else:
+            snap_obj = snap_obj + get_snapshots_by_name_recursively\
+                       (snapshot.childSnapshotList, snapname)
+    return snap_obj
 
 for vm in vms:
     if vm.name in vm_names:
-        print vm.name
-        snapshots = vm.snapshot.rootSnapshotList
-        for snapshot in snapshots:
-            if snapshot_name == snapshot.name:
-                snap_obj = snapshot.snapshot
-                print "Reverting snapshot:", snapshot.name, "for vm:", vm.name
-                task = [snap_obj.RevertToSnapshot_Task()]
-                WaitForTasks(task, connection)
+        snap_obj = get_snapshots_by_name_recursively\
+                   (vm.snapshot.rootSnapshotList, snapshot_name)
+        print("Reverting snapshot:", snap_obj[0].name, "for vm:", vm.name)
+        task = [snap_obj[0].snapshot.RevertToSnapshot_Task()]
+        WaitForTasks(task, connection)
 
 Disconnect(connection)
